3a770a36553d4bef708c6fd81448d6340dbcce3e
Leonid Vinikov <leonid@elementor.com>
2020-04-24 15:36:04 +0300

    Frontend/Components/Catalog: Fix - Product JSCS.

--- frontend/components/catalog/product.js
+++ frontend/components/catalog/product.js
@@ -11,8 +11,8 @@ import {
  * @memberOf components
  */
 export class Product extends Component {
-	constructor( parent, options ) {
-		super( parent, options );
+    constructor( parent, options ) {
+        super( parent, options );
 
         /**
          * @type {modules.Logger}
@@ -33,10 +33,10 @@ export class Product extends Component {
             catalog: options.api.catalog,
         }
 
-		this.events = {
-			onProductAdd: ( product ) => {},
-			onProductChange: ( product, amount ) => {},
-		};
+        this.events = {
+            onProductAdd: ( product ) => {},
+            onProductChange: ( product, amount ) => {},
+        };
 
         // After render.
         this.afterRender = () => {
@@ -46,62 +46,62 @@ export class Product extends Component {
                 amount: this.view.element.element.querySelector( '.amount' ),
             }
         };
-	}
+    }
 
-	static getNamespace() {
-		return 'Components/Catalog'
-	}
+    static getNamespace() {
+        return 'Components/Catalog'
+    }
 
-	static getName() {
-		return 'Components/Catalog/Product';
-	}
+    static getName() {
+        return 'Components/Catalog/Product';
+    }
 
-	setAmount( amount ) {
-	    if ( this.logger ) {
+    setAmount( amount ) {
+        if ( this.logger ) {
             this.logger.startWith( { amount } );
         }
 
-		this.elements.amount.value = amount;
-	}
+        this.elements.amount.value = amount;
+    }
 
-	onProductAdd( e ) {
+    onProductAdd( e ) {
         if ( this.logger ) {
             this.logger.startWith( { e } );
         }
 
-		const id = parseInt( this.options.id ),
-			amount = parseInt( this.elements.amount.value );
+        const id = parseInt( this.options.id ),
+            amount = parseInt( this.elements.amount.value );
 
-		let product = this.apis.catalog.getLocalProductById( id );
+        let product = this.apis.catalog.getLocalProductById( id );
 
-		// Assign `id` and `amount`.
-		Object.assign( product, { id, amount } );
+        // Assign `id` and `amount`.
+        product = Object.assign( {}, product, { id, amount } );
 
-		// Call callback
-		this.events.onProductAdd( product );
+        // Call callback
+        this.events.onProductAdd( product );
 
-		// Put it back to 1.
+        // Put it back to 1.
         this.setAmount( 1 );
-	}
+    }
 
-	onProductChange( e ) {
-	    if ( this.logger ) {
+    onProductChange( e ) {
+        if ( this.logger ) {
             this.logger.startWith( { e } );
         }
 
-		const val = e.currentTarget.value;
+        const val = e.currentTarget.value;
 
         if ( this.logger ) {
             this.logger.debug( `val: '${val}'` );
         }
 
-		this.events.onProductChange( this, parseInt( val ) );
-	}
+        this.events.onProductChange( this, parseInt( val ) );
+    }
 
-	template() {
-		const { id, name, price } = this.options;
+    template() {
+        const { id, name, price } = this.options;
 
-		return (`
+        return (`
             <div class="product">
                 <img src="img/product-${id}.jpg" alt="product-${id}" />
                 <h4 class="name color-secondary">${name}</h4>
@@ -116,31 +116,31 @@ export class Product extends Component {
                 </div>
             </div>
         `);
-	}
+    }
 
-	/**
-	 * Function on() : Declare event callback
-	 *
-	 * @param {'product:add'|'product:change'} event
-	 * @param {{function()}} callback
-	 */
-	on( event, callback ) {
-		switch ( event ) {
-			case 'product:add': {
-				this.events.onProductAdd = callback;
-			}
-			break;
+    /**
+     * Function on() : Declare event callback
+     *
+     * @param {'product:add'|'product:change'} event
+     * @param {{function()}} callback
+     */
+    on( event, callback ) {
+        switch ( event ) {
+            case 'product:add': {
+                this.events.onProductAdd = callback;
+            }
+                break;
 
-			case 'product:change': {
-				this.events.onProductChange = callback;
-			}
-			break;
+            case 'product:change': {
+                this.events.onProductChange = callback;
+            }
+                break;
 
-			default: {
-				alert( `${this.constructor.name}::on() -> invalid event type: '${event}'` );
-			}
-		}
-	}
+            default: {
+                alert( `${this.constructor.name}::on() -> invalid event type: '${event}'` );
+            }
+        }
+    }
 }
 
 export default Product;
