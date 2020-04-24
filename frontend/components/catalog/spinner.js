4f5113d4163998ee85a6612d1927811407ceefdc
Leonid Vinikov <leonid@elementor.com>
2020-04-24 15:50:31 +0300

    Frontend/Catalog/Spinner: Remove - Useless logger.

--- frontend/components/catalog/spinner.js
+++ frontend/components/catalog/spinner.js
@@ -1,25 +1,15 @@
 /**
  * @file: components/pagination.js
  * @author: Leonid Vinikov <czf.leo123@gmail.com>
- * @description: Manages pages
  */
 
-import * as services from 'SERVICES';
-import { Component, Logger } from 'MODULES';
+import { Component } from 'MODULES';
 import './spinner.css';
 
 /**
  * @memberOf components
  */
 export class Spinner extends Component {
-    constructor( parent, options ) {
-        super( parent, options );
-
-        this.logger = new Logger( Spinner.getName(), true );
-        this.logger.setOutputHandler( services.Terminal.onOutput );
-        this.logger.startWith( { options } );
-    }
-
     static getNamespace() {
         return 'Components/Catalog'
     }
