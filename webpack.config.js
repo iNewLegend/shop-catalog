74c638e846fec1219caa427a770500def388d264
Leonid Vinikov <czf.leo123@gmail.com>
2020-04-24 16:03:13 +0300

    Webpack: Fix - Update CS.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require( 'path' );
 
 module.exports = {
-	mode: 'development',
-	devtool: 'inline-source-map',
-	watch: true,
-	entry: {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    watch: true,
+    entry: {
         app: './frontend/app.js',
         mvc: './dev/mvc/index.js'
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'frontend/dist')
+        path: path.resolve( __dirname, 'frontend/dist' )
     },
     module: {
         rules: [
@@ -19,32 +19,32 @@ module.exports = {
                 use: ['style-loader', 'css-loader'],
             },
             {
-              test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env'],
-                  plugins: ['@babel/plugin-proposal-object-rest-spread']
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread']
+                    }
                 }
-              }
             },
-          ]
+        ]
     },
-	resolve: {
-    		// TODO: Read jsconfig.json for aliases.
-		alias: {
-			'API': path.resolve(__dirname, 'frontend/api'),
-			'COMPONENTS': path.resolve(__dirname, 'frontend/components'),
-            		'CORE': path.resolve(__dirname, 'frontend/core'),
-			'LIBRARY': path.resolve(__dirname, 'frontend/library'),
-			'MODULES': path.resolve(__dirname, 'frontend/modules'),
-			'PAGES': path.resolve(__dirname, 'frontend/pages'),
-			'SERVICES': path.resolve(__dirname, 'frontend/services'),
-			'DEV-LIBRARY': path.resolve(__dirname, 'dev/library'),
-			'DEV-MODULES': path.resolve(__dirname, 'dev/modules'),
-		}
-	},
+    resolve: {
+        // TODO: Read jsconfig.json for aliases.
+        alias: {
+            'API': path.resolve( __dirname, 'frontend/api' ),
+            'COMPONENTS': path.resolve( __dirname, 'frontend/components' ),
+            'CORE': path.resolve( __dirname, 'frontend/core' ),
+            'LIBRARY': path.resolve( __dirname, 'frontend/library' ),
+            'MODULES': path.resolve( __dirname, 'frontend/modules' ),
+            'PAGES': path.resolve( __dirname, 'frontend/pages' ),
+            'SERVICES': path.resolve( __dirname, 'frontend/services' ),
+            'DEV-LIBRARY': path.resolve( __dirname, 'dev/library' ),
+            'DEV-MODULES': path.resolve( __dirname, 'dev/modules' ),
+        }
+    },
     stats: {
         colors: true
     },
