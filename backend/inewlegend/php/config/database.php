a3f22af9354d1d9c8a2c361bb5180c9af3ff83d5
Leonid <czf.leo123@gmail.com>
2021-04-25 20:47:34 +0300

    Backend/iNewLegend/Config/Database: Fix - Use initial template.

--- backend/inewlegend/php/config/database.php
+++ backend/inewlegend/php/config/database.php
@@ -9,8 +9,8 @@ namespace Config;
 class Database {
 	const IS_CONFIGURED = false;
 
-	const HOST = 'localhost';
-	const NAME = 'shop_catalog';
-	const USERNAME = 'admin';
-	const PASSWORD = 'mysql';
+	const HOST = '%DB_HOST%';
+	const NAME = '%DB_NAME%';
+	const USERNAME = '%DB_USER%';
+	const PASSWORD = '%DB_PASS%';
 }
