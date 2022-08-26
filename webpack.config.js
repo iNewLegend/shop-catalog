32dd3f2ba6dc492d19be1749db71bc20cb7b7a43
Leonid Vinikov <czf.leo123@gmail.com>
2022-08-26 09:30:51 +0300

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
 const path = require( 'path' );
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    watch: true,
-    entry: {
-        app: './frontend/app.js',
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve( __dirname, 'frontend/dist' )
-    },
+	mode: 'development',
+	devtool: 'inline-source-map',
+	watch: true,
+	entry: {
+		app: './frontend/app.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve( __dirname, 'frontend/dist' )
+	},
 	devServer: {
 		contentBase: 'frontend/dist'
 	},
 	module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-	        {
-		        test: /\.(ts|js)x?$/,
-		        exclude: /node_modules/,
-		        use: {
-			        loader: 'babel-loader',
-			        options: {
-				        presets: [
-				        	'@babel/preset-env',
-					        '@babel/preset-typescript'
-				        ],
-				        plugins: [
-				        	[ '@babel/plugin-proposal-object-rest-spread' ],
-					        [ "@babel/plugin-transform-react-jsx", { "pragma": "new $flow.JsxElement" } ]
-				        ]
-			        }
-		        }
-	        },
-        ]
-    },
-    resolve: {
-	    extensions: ['.tsx', '.ts', '.js'],
-    },
-    stats: {
-        colors: true
-    },
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [ 'style-loader', 'css-loader' ],
+			},
+			{
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-env',
+							'@babel/preset-typescript'
+						],
+						plugins: [
+							[ '@babel/plugin-proposal-object-rest-spread' ],
+							[ "@babel/plugin-transform-react-jsx", { "pragma": "new $flow.JsxElement" } ]
+						]
+					}
+				}
+			},
+		]
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ],
+	},
+	stats: {
+		colors: true
+	},
 	externals: {
 		'@appflux/core': '$flow',
 		'@appflux/mvc': '$flow',
